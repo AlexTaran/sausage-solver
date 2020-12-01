3f3bdb0861dd774fff5b88b228cd648f5a1b9165
Alex <alexytaran@gmail.com>
2020-12-01 10:23:22 +0100

    Implement sausage

--- solver.cpp
+++ solver.cpp
@@ -1,4 +1,7 @@
+#include <algorithm>
 #include <iostream>
+#include <unordered_set>
+#include <utility>
 #include <vector>
 
 using namespace std;
@@ -13,6 +16,10 @@ struct Vec {
   Vec operator+(const Vec& other) {
     return Vec(x + other.x, y + other.y);
   }
+
+  bool operator==(const Vec& other) {
+    return x == other.x && y == other.y;
+  }
 };
 
 class Sausage {
@@ -24,9 +31,74 @@ class Sausage {
   Sausage clone() const {
 	return Sausage(points);
   }
+
+  bool isVertical() const {
+    for (int i = 1 ; i < points.size(); ++i) {
+	  if (points[0].x != points[i].x) {
+	    return false;
+	  }
+	}
+	return true;
+  }
+
+  bool isHorizontal() const {
+    for (int i = 1 ; i < points.size(); ++i) {
+	  if (points[0].y != points[i].y) {
+	    return false;
+	  }
+	}
+	return true;
+  }
+
+  bool isBurned() const {
+    for (const auto& g: grilled) {
+	  if (g.first > 1 || g.second > 1) {
+	    return true;
+	  }
+	}
+	return false;
+  }
+
+  bool isReady() const {
+    for (const auto& g: grilled) {
+	  if (g.first != 1 || g.second != 1) {
+	    return false;
+	  }
+	}
+	return true;
+  }
+ 
+  void Roll(const Vec& dv) {
+    for (auto& p: points) {
+      p = p + dv;
+    }
+    if ((isHorizontal() && dv.y % 2 == 1) || (isVertical() && dv.x % 2 == 1)) {
+      for (auto& g: grilled) {
+		swap(g.first, g.second);
+	  }
+    }
+  }
+
+  bool HasPoint(const Vec& p) {
+	return find(points.begin(), points.end(), p) != points.end();
+  }
+
+  template<template<class T> class Collection>
+  void grillByPoints(const Collection<Vec>& grillPoints) {
+    for (size_t i = 0; i < points.size(); ++i) {
+      if (grillPoints.contains(points[i])) {
+        grilled[i].first++;
+      }
+    }
+  }
+
+  const vector<Vec>& getPoints() const {
+    return points;
+  }
+
  private:
   vector<Vec> points;
-  vector<Vec> grilled;
+  vector<pair<int, int>> grilled; // first = bottom, second = top
 };
 
 int main() {
