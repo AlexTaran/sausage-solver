44a06c8da1c9dd0d3b0744e0e13f4a438340cae8
Alex <alexytaran@gmail.com>
2020-12-01 09:48:15 +0100

    More cpp stub

--- solver.cpp
+++ solver.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+struct Vec {
+  int x;
+  int y;
 
+  Vec() : x(0), y(0) { }
+  Vec(int x, int y) : x(x), y(y) { }
+
+  Vec operator+(const Vec& other) {
+    return Vec(x + other.x, y + other.y);
+  }
+};
+
+class Sausage {
+ public:
+  Sausage(const vector<Vec>& points) : points(points) {
+	grilled.resize(points.size());
+  }
+
+  Sausage clone() const {
+	return Sausage(points);
+  }
+ private:
+  vector<Vec> points;
+  vector<Vec> grilled;
+};
 
 int main() {
   cout << "Faster solver" << endl;
